9e8afd8aa4544d30c2c6e1ae1f4ef594cb109eb0
Ashley <31869932+awiles1@users.noreply.github.com>
2017-12-06 09:44:45 -0600

    cart6

--- js/jquery.mycart.js
+++ js/jquery.mycart.js
@@ -19,10 +19,6 @@
             clickOnCartIcon: function($cartIcon, products, totalPrice, totalQuantity) {},
             checkoutCart: function(products, totalPrice, totalQuantity) {
         var checkoutString = "Total Price: " + totalPrice + "\nTotal Quantity: " + totalQuantity;
-        checkoutString += "\n\n id \t name \t summary \t price \t quantity \t image path";
-        $.each(products, function(){
-          checkoutString += ("\n " + this.id + " \t " + this.name + " \t " + this.summary + " \t " + this.price + " \t " + this.quantity + " \t " + this.image);
-        });
         alert(checkoutString)
         console.log("checking out", products, totalPrice, totalQuantity);
       },
